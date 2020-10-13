a97816cde442ad2e1d6bfc84fec0a0a4f8144071
Samuel Emechebe <chijiokeemechebe@gmail.com>
2020-10-13 10:51:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,46 @@ function setGradient() {
         css.textContent = body.style.background + ";";
 }
 
+function copyToClickboard(){
+    var gradient = document.getElementById('gradient1').value
+    var gradient1rgb = `rgb(${hexToRgb(gradient).r},${hexToRgb(gradient).g},${hexToRgb(gradient).b})`
+    var gradient = document.getElementById('gradient2').value
+    var gradient2rgb = `rgb(${hexToRgb(gradient).r},${hexToRgb(gradient).g},${hexToRgb(gradient).b})`
+    var gradient = document.getElementById('gradient3').value
+    var gradient3rgb = `rgb(${hexToRgb(gradient).r},${hexToRgb(gradient).g},${hexToRgb(gradient).b})`
+
+    var copyText = `linear-gradient(to right, ${gradient1rgb}, ${gradient2rgb}, ${gradient3rgb});`
+    var node = document.createElement('textarea');
+    var selection = document.getSelection();
+
+    node.textContent = copyText;
+    document.body.appendChild(node);
+
+    selection.removeAllRanges();
+    node.select();
+    document.execCommand('copy');
+
+    selection.removeAllRanges();
+    document.body.removeChild(node);
+
+        alert('Copied to clickboard ' + copyText)
+}
+
+function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+      return r + r + g + g + b + b;
+    });
+  
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  }
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
